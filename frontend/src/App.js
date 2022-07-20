1548635ebd58db67484fae52c309aa08b7b023a0
RstTiwari <iamrrt2121@gmail.com>
2022-07-21 03:17:51 +0530

    FrondEnd created and Header component added

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js"
+import {BrowserRouter as Router , Route , Switch} from 'react-router-dom'
+
+function App() {
+  return (
+        <Router>
+          <Header />
+        </Router>
+  );
+}
+
+export default App;
